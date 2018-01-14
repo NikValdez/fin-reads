428a2fe856986dd0a4ecd05e99a79aa090d86a67
Nik <nerdvestor@gmail.com>
2018-01-13 16:31:32 -0800

    change feeds to feedjira and add footer

--- app/models/link.rb
+++ app/models/link.rb
@@ -2,35 +2,36 @@ class Link < ApplicationRecord
 
   def self.pcast_feed
   feed_urls = [
-    meb = FeedParser::Parser.parse( "http://mebfaber.libsyn.com/rss" ).items[0],
-    real_vision = FeedParser::Parser.parse( "http://adventuresinfinance.realvision.libsynpro.com/rss" ).items[0],
-    animal_spirits = FeedParser::Parser.parse( "http://animalspiritspod.libsyn.com/rss" ).items[0],
-    capital_allocators = FeedParser::Parser.parse( "http://tedseides.libsyn.com/rss" ).items[0],
-    invest_like_best = FeedParser::Parser.parse( "http://investlikethebest.libsyn.com/rss" ).items[0],
-    macro_voices = FeedParser::Parser.parse( "http://feeds.macrovoices.com/MacroVoices?format=xml" ).items[0],
+    meb = Feedjira::Feed.fetch_and_parse("http://mebfaber.libsyn.com/rss").entries[0],
+    real_vision = Feedjira::Feed.fetch_and_parse("http://adventuresinfinance.realvision.libsynpro.com/rss").entries[0],
+    animal_spirits = Feedjira::Feed.fetch_and_parse("http://animalspiritspod.libsyn.com/rss" ).entries[0],
+    capital_allocators = Feedjira::Feed.fetch_and_parse("http://tedseides.libsyn.com/rss" ).entries[0],
+    invest_like_best = Feedjira::Feed.fetch_and_parse("http://investlikethebest.libsyn.com/rss" ).entries[0],
+    macro_voices = Feedjira::Feed.fetch_and_parse("http://feeds.macrovoices.com/MacroVoices?format=xml").entries[0],
   ]
+
 
   end
 
   def self.blog_feed
     feed_urls = [
-      collab_fund = FeedParser::Parser.parse( "http://feeds.feedburner.com/collabfund" ).items[0],
-      alpha_architect = FeedParser::Parser.parse( "https://alphaarchitect.com/feed/" ).items[0],
-      meb_faber = FeedParser::Parser.parse( "http://mebfaber.com/feed/" ).items[0],
+      collab_fund = Feedjira::Feed.fetch_and_parse("http://feeds.feedburner.com/collabfund").entries[0],
+      alpha_architect = Feedjira::Feed.fetch_and_parse("https://alphaarchitect.com/feed/").entries[0],
+      meb_faber = Feedjira::Feed.fetch_and_parse("http://mebfaber.com/feed/" ).entries[0],
     ]
 
   end
 
   def self.paper_feed
     feed_urls = [
-      financial_econ = FeedParser::Parser.parse( "https://www.journals.elsevier.com/journal-of-financial-economics/rss" ).items[0],
+      financial_econ = Feedjira::Feed.fetch_and_parse("https://www.journals.elsevier.com/journal-of-financial-economics/rss").entries[0],
     ]
 
   end
 
   def self.quant_feed
     feed_urls = [
-      financial_econ = FeedParser::Parser.parse( "https://www.journals.elsevier.com/journal-of-financial-economics/rss" ).items[0],
+      financial_econ = Feedjira::Feed.fetch_and_parse("https://www.journals.elsevier.com/journal-of-financial-economics/rss").entries[0],
     ]
 
   end
