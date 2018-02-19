5575a2774c9af13a6065d36156447fbebe9ab475
Nik <nerdvestor@gmail.com>
2018-02-19 04:11:15 -0800

    oder by datetime

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,9 +1,9 @@
 class LinksController < ApplicationController
   def index
-    @podcasts = PcastFeed.all
-    @papers = PaperFeed.all
-    @blogs = BlogFeed.all
-    @quants = QuantFeed.all
+    @podcasts = PcastFeed.all.order("published_at DESC")
+    @papers = PaperFeed.all.order("published_at DESC")
+    @blogs = BlogFeed.all.order("published_at DESC")
+    @quants = QuantFeed.all.order("published_at DESC")
     @contact = Contact.new
   end
 end
