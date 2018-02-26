56b2042d03a3859d04412819697ff788e688c987
Nik <nerdvestor@gmail.com>
2018-02-26 01:00:39 -0800

    add paperfeeds

--- app/models/paper_feed.rb
+++ app/models/paper_feed.rb
@@ -2,19 +2,20 @@ class PaperFeed < ApplicationRecord
 
   def self.paper_feed
     feed_urls = [
-      financial_econ = Feedjira::Feed.fetch_and_parse("https://www.journals.elsevier.com/journal-of-financial-economics/rss").entries[0],
-      quantpedia = Feedjira::Feed.fetch_and_parse("https://quantpedia.com/Blog/Feed").entries[0],
-      academic_oup = Feedjira::Feed.fetch_and_parse("https://academic.oup.com/rss/site_5511/OpenAccess.xml").entries[0],
-      cfa_pubs = Feedjira::Feed.fetch_and_parse("https://www.cfapubs.org/action/showFeed?ui=0&mi=3bdplw&ai=t2&jc=rf&type=etoc&feed=rss").entries[0],
-      cfa_lit = Feedjira::Feed.fetch_and_parse("https://www.cfapubs.org/action/showFeed?ui=0&mi=3bydyv&ai=12u&jc=rflr&type=etoc&feed=rss").entries[0],
-      cfa_mag = Feedjira::Feed.fetch_and_parse("https://www.cfapubs.org/action/showFeed?ui=0&mi=3bdplw&ai=sz&jc=cfm&type=etoc&feed=rss").entries[0],
-      journal_of_finance = Feedjira::Feed.fetch_and_parse("http://journal.afajof.org/issue-archive/feed/").entries[0],
-      quant_news_papers = Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[0..9],
+      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[0],
+      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[1],
+      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[2],
+      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[3],
+      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[4],
+      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[5],
+      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[6],
+      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[7],
+      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[8]
 
     ]
 
   feed_urls.each do |entry|
-      unless exists? guid:  entry.id
+      unless exists? name: entry.title
         create!(
           name:         entry.title,
           summary:      entry.summary,
