a10bbaa611fd49bbcbdd4e84c98ffb224fc609e8
Nik <nerdvestor@gmail.com>
2018-03-11 06:52:00 -0700

    add new papers source and current date helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,9 @@ module ApplicationHelper
   def verbose_date(date)
     date.strftime('%B %d %Y')
   end
+
+
+  def now_date
+    DateTime.now.strftime('%B %d %Y')
+  end
 end
--- app/models/paper_feed.rb
+++ app/models/paper_feed.rb
@@ -2,20 +2,21 @@ class PaperFeed < ApplicationRecord
 
   def self.paper_feed
     feed_urls = [
-      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[0],
-      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[1],
-      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[2],
-      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[3],
-      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[4],
-      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[5],
-      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[6],
-      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[7],
-      Feedjira::Feed.fetch_and_parse("http://www.quantnews.com/feed_type/ssrn/feed/").entries[8]
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[0],
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[1],
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[2],
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[3],
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[4],
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[5],
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[6],
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[7],
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[8],
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[9],
+      Feedjira::Feed.fetch_and_parse("http://export.arxiv.org/rss/q-fin").entries[10],
 
     ]
 
   feed_urls.each do |entry|
-      unless exists? name: entry.title
         create!(
           name:         entry.title,
           summary:      entry.summary,
@@ -24,7 +25,6 @@ class PaperFeed < ApplicationRecord
           published_at: entry.published,
           guid:         entry.id
         )
-      end
     end
   end
 end
